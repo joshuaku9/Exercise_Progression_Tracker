b172fe466706e5e46b95a74d3f0b7bcf187415b0
joshuaku9 <joshuamku9@gmail.com>
2019-05-29 21:28:20 -0500

    Add login, signup, logout capablities. Create base layout for html files

--- workout/settings.py
+++ workout/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'exercises.apps.ExercisesConfig',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'workout.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR,],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
